abc79aba53c0a83397539eeb59cdfdba6624affc
Nazar Majewski <nldv12@gmail.com>
2023-05-03 09:58:02 +0200

    lets start

---
+++ Main.java
@@ -0,0 +1,15 @@
+package p2;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(()->new Windows());
+
+
+
+    }
+
+}
