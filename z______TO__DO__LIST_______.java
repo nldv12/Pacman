a9ccc564bc274f88c5fa826b119a06afc27179b6
Nazar Majewski <nldv12@gmail.com>
2023-05-07 16:38:08 +0200

    ready to start

---
+++ z______TO__DO__LIST_______.java
@@ -0,0 +1,62 @@
+package p2;
+
+public class z______TO__DO__LIST_______ {
+
+    /*
+
+    ------------- Ulepszenia
+        przeciwnicy raz na 5 sekund z prawdopodobieństwem 25% tworzą ulepszenia (np.
+        +50% prędkości poruszania się itp.), które gracz może zebrać. Zaimplementuj min. 5 różnych
+        niebanalnych ulepszeń.
+
+    ---- licznik punktów
+    ---- licznik czasu
+    ---- licznik żyć
+
+
+    ---- animacja rucu
+    ---- animacja jedzenia
+
+
+    ---- złożony skrót klawiszowy (Ctrl+Shift+Q) kończy grę i wraca do menu głównego
+
+
+    ---- okno z Prośbą o wpisanie nazwy gracza żeby wyświetlić go w rankingu
+
+    ---- trwałość rankingu wykorzystując interfejs Serializable i zapis do pliku.
+    ---- okno rankingu z paskami przewijania
+
+
+    Must Have :
+    ---- Wszystkie wyjątki mają być obsłużone odpowiednimi komunikatami
+    ---- Ranking ma wykorzystywać JList
+    ---- Okna Mają być skalowalne
+
+    Must Use :
+    ++++ Dziedziczenie
+    ---- Kolekcje
+    ---- Interfejsy lub klasy abstrakcyjne
+    ++++ Lambda wyrażenia
+    ---- Typy generyczne
+
+
+
+    Must Not :
+    ---- nie można używać narzędzi WYSIWYG (tzw.Window Builder’ów)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    */
+}
---
+++ z______Test_________.java
@@ -0,0 +1,9 @@
+package p2;
+
+public class z______Test_________ {
+    public static void main(String[] args) {
+
+
+    }
+}
+
